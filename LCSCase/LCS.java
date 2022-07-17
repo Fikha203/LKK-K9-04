ca74227d2b8da252dc4fb4ff006a8acaa4340bde
TaufikHakim <93898691+Fikha203@users.noreply.github.com>
2022-07-17 21:46:31 +0700

    Update
    Progra interaktif

--- LCSCase/LCS.java
+++ LCSCase/LCS.java
@@ -1,13 +1,45 @@
 package LCSCase;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LCS {
     public static void main(String[] args) {
-        char[] x = {'A', 'B', 'C', 'B', 'D', 'A','B'};
-        char[] y = {'B', 'D', 'C', 'A', 'B', 'A'};
+        Scanner input = new Scanner(System.in);
+        String perintah = "";
+        do{
+            System.out.println("==================================================");
+            System.out.println("Program mencari LCS dari dua buah string");
+            System.out.println("==================================================");
+            System.out.println("Selamat datang di program kami!!!");
+            System.out.println("Silahkan mengisi data-data berikut ini\n");
 
-        ArrayList z = findLCS(x,y);
+            System.out.print("Masukkan String pertama : ");
+            String s1 = input.next();
+            System.out.print("Masukkan String kedua : ");
+            String s2 = input.next();
+
+            System.out.println("\n----------Jawaban ditemukan----------");
+            
+            char[] x = s1.toCharArray();
+            char[] y = s2.toCharArray();
+            ArrayList z = findLCS(x,y);
+            print(z);
+            System.out.printf("LCS dari String \"%s\" dan String \"%s\" : ", s1, s2);
+
+            print_LCS((String[][]) z.get(1) , x,x.length,y.length);
+            System.out.println();
+            
+            System.out.println("----------------------------------------------------");
+            System.out.println("Apakah anda ingin menggunakan program ini lagi?(Y/N)");
+            System.out.println("----------------------------------------------------");
+            perintah = input.next();
+        } while(perintah.equalsIgnoreCase("Y"));
+        System.out.println("==================================================");
+        System.out.println("Terima kasih telah menggunakan program kami");
+        
+
 
         
     }
@@ -38,15 +70,47 @@ public class LCS {
 
                 } else {
                     c[i][j] = c[i][j - 1];
-                    b[i][j] = "<|";
+                    b[i][j] = "<-";
                 }
             }
         }
-        print(c);
-        print(b);
-        return new ArrayList<>();
+        ArrayList temp = new ArrayList();
+        temp.add(c);
+        temp.add(b);
+
+        return temp;
+    }
+    static void print_LCS(String[][] b, char[]x,int i,int j){
+        if(i == 0 || j == 0){
+            return ;
+        }
+        if(b[i][j].equals("^\\")){
+            print_LCS(b, x, i-1, j-1);
+            System.out.print(x[i-1]);
+
+        } else if(b[i][j].equals("^|")){
+            print_LCS(b, x, i-1, j);
+        } else {
+            print_LCS(b, x, i, j-1);
+        }
+    }
+    
+
+    static void print(ArrayList p){
+        for (Object object : p) {
+            if(object.getClass() == String[][].class){
+                String[][] s = (String[][]) object;
+                print(s);
+            } else if (object.getClass() == int[][].class){
+                int[][] s = (int[][]) object;
+                print(s);
+            }
+        }
     }
     static void print(int[][] p){
+        System.out.println("\n--------------");
+        System.out.println("Matriks Angka");
+        System.out.println("--------------");
         for (int i = 0; i < p.length; i++) {
             for (int j = 0; j < p[i].length; j++) {
                 System.out.print(p[i][j] + " ");
@@ -55,11 +119,18 @@ public class LCS {
         }
         System.out.println();
     }
-
+    
     static void print(String[][] p){
+        System.out.println("--------------");
+        System.out.println("Matriks Panah");
+        System.out.println("--------------");
         for (int i = 0; i < p.length; i++) {
             for (int j = 0; j < p[i].length; j++) {
-                System.out.print(p[i][j] + " ");
+                if(p[i][j] == null){
+                    System.out.print(" - ");
+                } else {
+                    System.out.print(p[i][j] + " ");
+                }
             }
             System.out.println();
         }
--- MCMCase/MCM.java
+++ MCMCase/MCM.java
@@ -2,66 +2,157 @@ package MCMCase;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MCM {
 
     public static void main(String[] args) {
-        int[] p = {30, 35, 15, 5, 10, 20, 25};
-        ArrayList<int[][]> hasil = MatrixChainOrder(p);
-        print(hasil);
+        Scanner input = new Scanner(System.in);
+
+        // String perintah yang nantinya untuk menentukan apakah loop berhenti atau tidak
+        String perintah = "";
+        do{
+            System.out.println("==================================================");
+            System.out.println("Program mencari jumlah perkalian minimum matriks");
+            System.out.println("==================================================");
+            System.out.println("Selamat datang di program kami!!!");
+            System.out.println("Silahkan mengisi data-data berikut ini\n");
+            System.out.print("Masukkan jumlah matriks : ");
+            int n = input.nextInt();
+
+            // ArrayList dari matriks-matriks yang diinputkan
+            ArrayList<Integer> matriks[] = new ArrayList[n+1];
+            for (int i = 0; i < matriks.length; i++) {
+                matriks[i] = new ArrayList<Integer>();
+            }
+            int[] p = new int[n+1];
+
+            System.out.println("Masukkan data baris dan kolom tiap-tiap matriks (baris<spasi>kolom)");
+            System.out.println("Matriks 1 : ");
+            p[0] = input.nextInt();
+            p[1] = input.nextInt();
+            matriks[1].add(p[0]);
+            matriks[1].add(p[1]);
+            for(int i = 2; i<= n; i++){
+                System.out.println("Matriks " + i + " : ");
+                int baris = input.nextInt();
+                int kolom = input.nextInt();
+                p[i] = kolom;
+                matriks[i].add(baris);
+                matriks[i].add(kolom);
+            }
+            System.out.println("\n----------Jawaban ditemukan----------");
+            
+            ArrayList<int[][]> hasil = findMCM(p);
+            print(hasil);
+
+            do{
+                System.out.println("==================================================");
+                System.out.println("\nData matriks : \n");
+
+                for (int i = 1; i <= n; i++) {
+                    System.out.printf("Matriks ke-%d : %d X %d\n", i,matriks[i].get(0),matriks[i].get(1));
+                }
+
+                System.out.println("\nPilih matriks awal dan tujuan untuk dihitung");
+                System.out.print("Matriks awal : " );
+                int i = input.nextInt();
+                System.out.print("Matriks tujuan : " );
+                int j = input.nextInt();
+
+                System.out.printf("\nSolusi optimal perkalian matriks dari matriks ke-%d sampai matriks ke-%d adalah : \n",i,j);
+
+                System.out.printf("Jumlah cost minimum perkalian : %d\n", hasil.get(0)[i][j]);
+
+                System.out.print("Pengukurungan perkalian matriks yang optimal : ");
+                print_Optimal(hasil.get(1), i, j);
+
+                System.out.println("\n#note : M1 = Matriks ke-1");
+
+                System.out.println("----------------------------------------------");
+                System.out.println("Apakah anda ingin memilih matriks ulang?(Y/N)");
+                perintah = input.next();
+                System.out.println("----------------------------------------------");
+                
+            } while(perintah.equals("Y"));
+            
+            System.out.println("\n ");
+            System.out.println("----------------------------------------------");
+            System.out.println("Apakah anda ingin menggunakan program ini lagi?(Y/N)");
+            System.out.println("----------------------------------------------");
+            perintah = input.next();
+            
+        } while(perintah.equalsIgnoreCase("Y"));
+        System.out.println("==================================================");
+        System.out.println("Terima kasih telah menggunakan program kami");
+    
     }
+    
+    static ArrayList<int[][]> findMCM(int[] p) {
+        int n = p.length;
 
-    static ArrayList<int[][]> MatrixChainOrder(int[] p) {
-        int n = p.length - 1;
+        int i,j,k,l,q;
+        int[][] m = new int[n][n];
+        int[][] s = new int[n][n];
 
-        int[][] m = new int[n + 1][n + 1];
-        int[][] s = new int[n + 1][n + 1];
-
-        for (int i = 0; i < n; i++) {
+        for (i = 1; i < n; i++) {
             m[i][i] = 0;
         }
 
-        int j = 0;
-        int q = 0;
-
-        for (int l = 2; l <= n; l++) {
-            for (int i = 1; i <= n - l + 1; i++) {
+        for (l = 2; l < n; l++) {
+            for (i = 1; i < n - l + 1; i++) {
                 j = i + l - 1;
                 m[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k <= j - 1; k++) {
+                for (k = i; k <= j - 1; k++) {
                     q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
                     if (q < m[i][j]) {
                         m[i][j] = q;
                         s[i][j] = k;
                     }
-                    print(m);
-                    print(s);
                 }
             }
         }
         return new ArrayList<>(Arrays.asList(m, s));
     }
 
+    static void print_Optimal(int[][] s, int i, int j){
+        if(i == j){
+            System.out.print("M" + i);
+        } else {
+            System.out.print("(");
+            print_Optimal(s, i, s[i][j]);
+            print_Optimal(s, s[i][j]+1, j);
+            System.out.print(")");
+        }
+    }
+
     static void print(ArrayList<int[][]> p) {
         for (int[][] a : p) {
-            for (int i = 0; i < a.length; i++) {
-                for (int j = 0; j < a[i].length; j++) {
-                    System.out.print(a[i][j] + " ");
-                }
-                System.out.println("");
+            System.out.print(" |");
+            for(int j = 1;j< a.length ; j++){
+                System.out.printf("%d\t|",j);
             }
-            System.out.println("\n");
+            for (int i = a.length-1; i >= 1; i--) {
+                System.out.println("\n--------------------------------------------------");
+                System.out.printf("%d|",i);
+                for (int j = 1;j< a[i].length ; j++) {
+                    System.out.printf("%d\t|",a[j][i]);
+                    
+                }
+            }
+            System.out.println("\n\n");
         }
     }
 
     static void print(int[][] p) {
-        for (int i = 0; i < p.length; i++) {
-            for (int j = 0; j < p[i].length; j++) {
+        for (int i = p.length-1; i >= 1; i--) {
+            for (int j = 1; j < p[i].length; j++) {
                 System.out.print(p[i][j] + " ");
             }
             System.out.println("");
         }
         System.out.println("");
     }
+
 }
 
